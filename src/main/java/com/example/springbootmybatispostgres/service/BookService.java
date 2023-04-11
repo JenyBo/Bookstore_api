6d200bd3d47f8b4772a59207138b53bffff0a3e9
NguyenQuangTruong <quangtruongyb2002@gmail.com>
2023-04-11 15:50:03 +0700

    first commit

---
+++ src/main/java/com/example/springbootmybatispostgres/service/BookService.java
@@ -0,0 +1,20 @@
+package com.example.springbootmybatispostgres.service;
+
+import com.example.springbootmybatispostgres.entities.Book;
+
+import java.util.List;
+
+public interface BookService {
+
+    Book create(Book book);
+
+    List<Book> getAll();
+
+    Book getOne(long id);
+
+    void deleteById(long id);
+
+    Book getByTitle(String title);
+
+    void updateById( long id , Book book);
+}
